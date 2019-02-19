fa7538f4b4e54b125d938d40deb2a8783ec64e3b
Kieran Wood <kieranw098@gmail.com>
2019-02-19 01:38:43 -0700

    Continued Sketching

--- game.py
+++ game.py
@@ -1,60 +1,49 @@
 import curses
-
-# Keybinds
-
-# Board/Map Class
-class board:
-    def __init__(self, width= 120, height = 20, char="█"):
-        self.height = height
-        self.width = width
-        self.char = char
-        self.board = self.generate_board(char, width, height)
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN, KEY_EXIT  # Keybinds
+import random
+import time
 
 
-    def generate_board(self, char, width, height):
-        board = []
-        tb = char*width # Used to generate the top and bottom of the board
-        mid = char + (" "*(width-2)) + char # Used to generate the sections in between the top and bottom
-        board.append(tb) # Create topmost line
-        for i in range(height): # Create middle of map
-            board.append(mid)
-        board.append(tb) # Create Bottommost line
-        return board
-    
-    def __radd__(self, other):
-        pass
-
-# Snake class
 class snake:
     def __init__(self):
         pass
 
     def __add__(self, other):
         pass
-    
+
     def __radd__(self, other):
         pass
 
 
-# Food
 class food:
-    def __init__(self):
-        pass
+    def __init__(self, max_x=25, max_y=60, char="█", coordinates=[20, 20]):
+        """ """
+        self.char = char
+        self.coordinates = coordinates
 
-    def __add__(self, other):
-        pass
-    
-    def __radd__(self, other):
-        pass
-
-
-# Gameloop
+    def regenerate():
+        """Regenerates the food coordinates"""
+        self.coordinates = [random.randint(0,self.max_x),random.randint(0,self.maxy)]
 
 
+def game_loop():
+    # Window initialization
+    main = curses.initscr()
+    height, width = main.getmaxyx()  # Gets the max window size
+    window = curses.newwin(height, width, 0, 0)  # Creates a mew window drawn max x and y size from the top left
+    curses.noecho()  # Turns off key echoing
+    curses.curs_set(0)  # Hides Cursor
+    main.border(0)  # Adds a boarder to window
+    main.nodelay(1)
 
-# Testing
+    a = food()
+
+    # Actual Game Loop
+    while(True):  # TODO: Make game run while not esc pressed or game over
+        main.refresh()
+        window.addch(a.coordinates[0], a.coordinates[1], a.char) 
+        a.regenerate()
+        time.sleep(1)
 
 if __name__ == "__main__":
-    a = board()
-    for i in (a.board):
-        print(i)
+    game_loop()
