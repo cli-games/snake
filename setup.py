bb4530a0a9072852549c4ee916169f960d85e8ef
Kieran Wood <kieranw098@gmail.com>
2019-02-19 09:12:35 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+import pip
+
+if os.name() == 'nt':
+    # Run pip install -r requirements-win.txt
+    pass
+
+else:
+    # Run pip install -r requirements.txt
+    pass
+
+if not (__name__ == "__main__"):  # If running from launcher
+    pass
+
+if __name__ == "__main__":  # if running standalone
+    pass
+
